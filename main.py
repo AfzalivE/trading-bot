ed3f8199775369b82694b119d813180fc090b42d
Afzal Najam <afzalnaj@gmail.com>
2020-12-31 15:45:27 -0500

    Remove custom stock trader, move to bt

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 from pandas_datareader import data
 
 from bot.BtStockTrader import BtStockTrader
-from bot.StockTrader import StockTrader
 
 
 def print_hi(name):
     print(f'Hi, {name}')
-
-
-def run_stock_trader():
-    trader = StockTrader()
-    if trader.is_download_stale():
-        trader.download_data()
-    trader.run()
-    pass
 
 
 def run_bt_stock_trader():
@@ -22,5 +13,4 @@ def run_bt_stock_trader():
 
 if __name__ == '__main__':
     print_hi('PyCharm')
-    # run_stock_trader()
     run_bt_stock_trader()
